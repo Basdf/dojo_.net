77685d1c02208141b4c6e6de6aa65493dd4217e8
Eduardo Velasquez <eduardo.velasquezv@udea.edu.co>
2020-10-03 11:49:06 -0500

    Dojo finalizado

---
+++ Modelo/Usuario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace dojo_.net.Modelo
+{
+    public class Usuario
+    {
+        
+        public String Cedula{get;set;}
+        public String Nombre{get;set;}
+        public String Correo{get;set;}
+        public String Carrera{get;set;} 
+
+    }
+}
